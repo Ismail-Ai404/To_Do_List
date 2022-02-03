b4b9b24a4d1ac67abba09da3401eda73f78ab3c7
Smilexix_ <ayon000777@gmail.com>
2022-02-03 15:45:41 +0600

    ADD hello world

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const addForm = document.querySelector('.add');
-const search = document.querySelector('.search input');
-const list = document.querySelector('.todos');
-const doneList = document.querySelector('.done');
+const addForm = document.querySelector(".add");
+const search = document.querySelector(".search input");
+const list = document.querySelector(".todos");
+const doneList = document.querySelector(".done");
 
-
-const generateTemplate = todo => {
-  const html = `
+const generateTemplate = (todo) => {
+     const html = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <span>${todo}</span>
       
@@ -14,88 +13,77 @@ const generateTemplate = todo => {
 
     </li>
   `;
-  list.innerHTML += html;
+     list.innerHTML += html;
 };
-const addToDone= todo =>{
-  const html=`<li class="list-group-item d-flex justify-content-between align-items-center">
+const addToDone = (todo) => {
+     const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
       <span style="text-decoration: line-through">${todo}</span>
       <i class="far fa-trash-alt delete"></i>
-    </li>`
-    doneList.innerHTML+=html;
-}
-
-const filterTodos = term => {
-
-  // add filtered class
-  Array.from(list.children )
-    .filter(todo => !todo.textContent.toLowerCase().includes(term))
-    .forEach(todo => todo.classList.add('filtered'));
-
-  // remove filtered class
-  Array.from(list.children )
-    .filter(todo => todo.textContent.toLowerCase().includes(term))
-    .forEach(todo => todo.classList.remove('filtered'));
-
+    </li>`;
+     doneList.innerHTML += html;
 };
-const filterDone = term => {
 
-  // add filtered class
-  Array.from(doneList.children )
-    .filter(todo => !todo.textContent.toLowerCase().includes(term))
-    .forEach(todo => todo.classList.add('filtered'));
+const filterTodos = (term) => {
+     // add filtered class
+     Array.from(list.children)
+          .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+          .forEach((todo) => todo.classList.add("filtered"));
 
-  // remove filtered class
-  Array.from(doneList.children )
-    .filter(todo => todo.textContent.toLowerCase().includes(term))
-    .forEach(todo => todo.classList.remove('filtered'));
+     // remove filtered class
+     Array.from(list.children)
+          .filter((todo) => todo.textContent.toLowerCase().includes(term))
+          .forEach((todo) => todo.classList.remove("filtered"));
+};
+const filterDone = (term) => {
+     // add filtered class
+     Array.from(doneList.children)
+          .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+          .forEach((todo) => todo.classList.add("filtered"));
 
+     // remove filtered class
+     Array.from(doneList.children)
+          .filter((todo) => todo.textContent.toLowerCase().includes(term))
+          .forEach((todo) => todo.classList.remove("filtered"));
 };
 
 // add todos event
-addForm.addEventListener('submit', e => {
-  
-  e.preventDefault();
-  const todo = addForm.add.value.trim();
+addForm.addEventListener("submit", (e) => {
+     e.preventDefault();
+     const todo = addForm.add.value.trim();
 
-  if(todo.length){
-    generateTemplate(todo);
-    addForm.reset();
-  }
-
+     if (todo.length) {
+          generateTemplate(todo);
+          addForm.reset();
+     }
 });
 
 // move todos event to done
-list.addEventListener('click', e => {
-//const store;
-  if(e.target.classList.contains('fa-check')){
-     
-     const store=e.target.parentElement;
-     e.target.parentElement.remove();
-     addToDone(store.textContent);
-          }
-     
-
+list.addEventListener("click", (e) => {
+     //const store;
+     if (e.target.classList.contains("fa-check")) {
+          const store = e.target.parentElement;
+          e.target.parentElement.remove();
+          addToDone(store.textContent);
+     }
 });
 
 // delete done event
-doneList.addEventListener('click', e => {
-//const store;
-  if(e.target.classList.contains('delete')){
-     //store=e.target.parentElement();
-     e.target.parentElement.remove();
+doneList.addEventListener("click", (e) => {
+     //const store;
+     if (e.target.classList.contains("delete")) {
+          //store=e.target.parentElement();
+          e.target.parentElement.remove();
      }
-     
-  
 
+     //This is to see if push works
 });
 
 // filter todos event
-search.addEventListener('keyup', () => {
-
-  const term = search.value.trim().toLowerCase();
-  filterTodos(term);
-  filterDone(term);
-
+search.addEventListener("keyup", () => {
+     const term = search.value.trim().toLowerCase();
+     filterTodos(term);
+     filterDone(term);
 });
 
 //checking method
+//Hello world!
